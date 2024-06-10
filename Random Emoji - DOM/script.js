4cc0aed822cd94e09524f169c49418d0ef7d0be1
Ebad <146643721+smebad@users.noreply.github.com>
2024-06-10 23:39:33 +0500

    Add files via upload

---
+++ Random Emoji - DOM/script.js
@@ -0,0 +1,36 @@
+const emoji = document.querySelector("#emoji");
+const emojis = [
+  "🤔",
+  "🤣",
+  "😍",
+  "😒",
+  "😁",
+  "😘",
+  "😢",
+  "😎",
+  "😉",
+  "🤗",
+  "😑",
+  "😴",
+  "🤨",
+  "🤑",
+  "🤩",
+  "🤯",
+  "😲",
+  "🥵",
+  "😱",
+  "🥱",
+  "😭",
+  "🤐",
+  "🤬",
+  "🤡",
+  "💀",
+  "💩",
+  "🥳",
+  "🤫",
+  "🥸",
+];
+
+emoji.addEventListener('mouseover', () => {
+  emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+});
